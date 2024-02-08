b7147c2b8cd1cdd2e490e1f8c78f178bef5359f6
anasahmd <anasahmad0239@gmail.com>
2024-02-08 19:04:17 +0530

    Add auto focus on input

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -14,10 +14,13 @@ const TextInput = ({
 			<input
 				className="w-full p-3 sm:p-4 rounded-2xl text-lg sm:text-2xl disabled:bg-white disabled:cursor-not-allowed dark:bg-gray-800"
 				type="text"
+				id="input"
 				value={inputText}
 				onChange={(e) => onKeyPress(e)}
 				disabled={isDisabled}
+				placeholder={isDisabled ? 'Press restart to test again!' : ''}
 				autoCapitalize="none"
+				ref={(input) => input && input.focus()}
 			/>
 		</>
 	);
